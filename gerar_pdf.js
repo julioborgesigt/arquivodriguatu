97c4cbf8362338c778952c065e4288c33d6fdfde
julioborgesigt <julio.aparecido3@email.com>
2024-10-07 19:23:17 -0300

    json

--- gerar_pdf.js
+++ gerar_pdf.js
@@ -115,7 +115,6 @@ app.post('/leitura', (req, res) => {
 
     console.log(horaAjustada);
 
-
     // Extrair o número do procedimento da URL do QR code
     const urlParams = new URLSearchParams(new URL(qrCodeMessage).search);
     const numeroProcedimento = urlParams.get('procedimento');
