873af971e7b2702530f225c00cdaea515c86eb03
julioborgesigt <julio.aparecido3@email.com>
2024-11-14 21:09:07 -0300

    json

--- public/script.js
+++ public/script.js
@@ -603,26 +603,26 @@ function converterProcedimento() {
 
 
  // Função para mostrar o formulário com efeito de transição
-function mostrarFormulario(formularioId) {
-    // Obter todos os formulários e ocultar os que estão visíveis
+ function mostrarFormulario(formularioId) {
+    // Seleciona todos os formulários para aplicar a transição de saída nos visíveis
     const formularios = document.querySelectorAll('.formulario');
     formularios.forEach(form => {
-        if (form.classList.contains('fade-in')) {
-            form.classList.remove('fade-in');
-            form.classList.add('fade-out');
-
+        if (form.classList.contains('slide-in')) {
+            form.classList.remove('slide-in');
+            form.classList.add('slide-out');
             // Aguarda a transição antes de ocultar completamente
             setTimeout(() => {
                 form.style.display = 'none';
-                form.classList.remove('fade-out');
-            }, 500); // Tempo de duração da transição em milissegundos
+                form.classList.remove('slide-out');
+            }, 500);
         }
     });
 
-    // Exibir o formulário selecionado com a classe de fade-in
+    // Exibe o formulário selecionado com a classe de slide-in
     const formularioSelecionado = document.getElementById(formularioId);
     formularioSelecionado.style.display = 'block';
     setTimeout(() => {
-        formularioSelecionado.classList.add('fade-in');
-    }, 10); // Delay para garantir que o efeito ocorra
+        formularioSelecionado.classList.add('slide-in');
+    }, 10);
 }
+
