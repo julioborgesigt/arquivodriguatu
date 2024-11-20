7f516fb5e472f098f7cebb6548155b08fcde58ca
julioborgesigt <julio.aparecido3@email.com>
2024-11-20 19:36:25 -0300

    json

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-const PORT = 3000;
+/*const PORT = 3000;*/
+
+
+const PORT = process.env.PORT || 3000;
+
 
 const cors = require('cors');
 app.use(cors());
