52fc1deea17257c9d1c7a3761806660db847db28
julioborgesigt <julio.aparecido3@email.com>
2024-11-23 18:35:41 -0300

    json

--- server.js
+++ server.js
@@ -714,6 +714,7 @@ app.post('/converterProcedimento', (req, res) => {
 
 
 
+
 // Rota para verificar se existe uma solicitação pendente para um procedimento
 app.get('/verificarSolicitacaoPendente', (req, res) => {
     const { procedimento } = req.query;
