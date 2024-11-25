cb91901ad343dc1fc3ca2d5efa358521592dd791
julioborgesigt <julio.aparecido3@email.com>
2024-11-25 17:39:13 -0300

    json

--- script.js
+++ script.js
@@ -251,7 +251,7 @@ function consultarMovimentacao() {
             if (data.success) {
                 let html = `<h3>Movimentações para o procedimento ${numeroCompleto}:</h3><ul>`;
                 data.leituras.forEach(leitura => {
-                    html += `<li>${leitura.usuario}, Data: ${leitura.data}, Hora: ${leitura.hora}</li>`;
+                    html += `<li>${leitura.usuario}, Data: ${leitura.data}, Hora: ${leitura.hora}, Observações:  ${data.observacoes}</li>`;
                 });
                 html += `</ul>`;
 
@@ -266,7 +266,7 @@ function consultarMovimentacao() {
             } else {
                 resultadoDiv.innerHTML = `<p>${data.message}</p>`;
             }
-            
+
         })
         .catch(error => {
             console.error('Erro ao consultar movimentação:', error);
